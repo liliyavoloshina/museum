efba9ae9ec42c32c4151af620afe73ca35816bb2
Liliya <liliyavoloshine@gmail.com>
2021-08-24 15:48:23 +0400

    feat: add progress bar watcher

--- museum/src/js/video.js
+++ museum/src/js/video.js
@@ -1,54 +1,64 @@
 const currentVideo = document.querySelector('#currentVideo')
-const currentSource = document.querySelector('#currentSource')
+  const currentSource = document.querySelector('#currentSource')
 
-const videoCurrentMainBtn = document.querySelector('#videoCurrentMainBtn')
-const videoCurrentSmallBtn = document.querySelector('#videoCurrentSmallBtn')
+  const videoCurrentMainBtn = document.querySelector('#videoCurrentMainBtn')
+  const videoCurrentSmallBtn = document.querySelector('#videoCurrentSmallBtn')
 
-const videoRangeProgress = document.querySelector('#video-range-progress')
-const videoRangeVolume = document.querySelector('#video-range-volume')
+  const videoRangeProgress = document.querySelector('#video-range-progress')
+  const videoRangeVolume = document.querySelector('#video-range-volume')
 
-let currentVideoSrc = './video/video0.mp4'
+  let currentVideoSrc = './video/video0.mp4'
+  // let currentVideoDuration = +currentVideo.duration
+  let currentVideoDuration
 
-currentSource.setAttribute('src', currentVideoSrc)
-currentVideo.load()
+  currentSource.setAttribute('src', currentVideoSrc)
+  // console.log(currentVideoDuration)
+  // currentVideo.setAttribute('max', Math.floor(currentVideo.duration))
+  currentVideo.load()
 
-videoRangeProgress.max
 
-currentVideo.addEventListener('click', togglePlayBtn)
-videoCurrentMainBtn.addEventListener('click', togglePlayBtn)
-videoCurrentSmallBtn.addEventListener('click', togglePlayBtn)
+  // videoRangeProgress.max
 
-videoRangeProgress.addEventListener('input', handleInputChange)
-videoRangeProgress.addEventListener('input', handleProgress)
-videoRangeVolume.addEventListener('input', handleInputChange)
+  currentVideo.addEventListener('click', togglePlayBtn)
+  videoCurrentMainBtn.addEventListener('click', togglePlayBtn)
+  videoCurrentSmallBtn.addEventListener('click', togglePlayBtn)
 
-currentVideo.addEventListener('timeupdate', watchProgess)
+  videoRangeProgress.addEventListener('input', handleInputChange)
+  videoRangeProgress.addEventListener('input', handleProgress)
+  videoRangeVolume.addEventListener('input', handleInputChange)
 
-function handleInputChange(e) {
-  const value = e.target.value;
-  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
-}
+  currentVideo.addEventListener('timeupdate', watchProgess)
 
-function togglePlayBtn() {
-  if (currentVideo.paused) {
-    currentVideo.play()
-    videoCurrentMainBtn.classList.add('hidden')
-    videoCurrentSmallBtn.classList.add('paused')
-  } else {
-    currentVideo.pause()
-    videoCurrentMainBtn.classList.remove('hidden')
-    videoCurrentSmallBtn.classList.remove('paused')
+  function handleInputChange(e) {
+    const value = e.target.value
+    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
   }
-}
 
-function handleProgress() {
-  const newTime = this.value / 100 * currentVideo.duration
-  currentVideo.currentTime = newTime
-  currentVideo.play()
-}
+  function togglePlayBtn() {
+    if (currentVideo.paused) {
+      currentVideo.play()
+      videoCurrentMainBtn.classList.add('hidden')
+      videoCurrentSmallBtn.classList.add('paused')
+    } else {
+      currentVideo.pause()
+      videoCurrentMainBtn.classList.remove('hidden')
+      videoCurrentSmallBtn.classList.remove('paused')
+    }
+  }
 
-function watchProgess() {
-  console.log(videoRangeProgress.value, 'videoRangeProgress.value')
-  console.log(currentVideo.currentTime, 'currentVideo.currentTime')
-  // videoRangeProgress.value = currentVideo.currentTime
-}
+  function handleProgress() {
+    const newTime = (this.value / 100) * currentVideo.duration
+    currentVideo.currentTime = newTime
+    currentVideo.play()
+  }
+
+  function watchProgess() {
+    const newValue = Math.round(100 / this.duration * this.currentTime)
+    videoRangeProgress.value = newValue
+    videoRangeProgress.style.background = `linear-gradient(to right, #710707 0%, #710707 ${newValue}%, #c4c4c4 ${newValue}%, #c4c4c4 100%)`
+
+    if (this.currentTime === this.duration) {
+      videoCurrentMainBtn.classList.remove('hidden')
+      videoCurrentSmallBtn.classList.remove('paused')
+    }
+  }
