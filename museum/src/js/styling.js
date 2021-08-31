043de193ac858c2eabc58ccdd0e1b60657e32765
Liliya <liliyavoloshine@gmail.com>
2021-08-31 11:40:48 +0400

    feat: add ripple

--- museum/src/js/styling.js
+++ museum/src/js/styling.js
@@ -1,23 +1,23 @@
 const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
-    button.addEventListener('click', function (e) {
-        const x = e.clientX
-        const y = e.clientY
+  button.addEventListener('click', function (e) {
+    const target = e.target
 
-        const buttonTop = e.target.offsetTop
-        const buttonLeft = e.target.offsetLeft
+    // Get the bounding rectangle of target
+    const rect = target.getBoundingClientRect()
 
-        const xInside = x - buttonLeft
-        const yInside = y - buttonTop
+    // Mouse position
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
 
-        const circle = document.createElement('span')
-        circle.classList.add('circle')
-        circle.style.top = yInside + 'px'
-        circle.style.left = xInside + 'px'
+    const circle = document.createElement('span')
+    circle.classList.add('circle')
+    circle.style.top = y + 'px'
+    circle.style.left = x + 'px'
 
-        this.appendChild(circle)
+    this.appendChild(circle)
 
-        // setTimeout(() => circle.remove(), 500)
-    })
+    setTimeout(() => circle.remove(), 500)
+  })
 })
