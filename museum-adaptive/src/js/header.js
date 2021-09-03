97bb7fd4a963f5e7c6bec99d3c099167dce22676
Liliya <liliyavoloshine@gmail.com>
2021-09-03 17:46:31 +0400

    feat: add navbar

--- museum-adaptive/src/js/header.js
+++ museum-adaptive/src/js/header.js
@@ -1,11 +1,17 @@
-const headerBtn = document.querySelector('#headerBtn')
+const headerBtn = document.querySelector('#headerBtn'),
+headerNav = document.querySelector('#headerNav')
 
 headerBtn.addEventListener('click', toggleHeader)
 
 function toggleHeader() {
   changeImage()
+  toggleNav()
 }
 
 function changeImage() {
   headerBtn.classList.toggle('opened')
+}
+
+function toggleNav() {
+  headerNav.classList.toggle('opened')
 }
