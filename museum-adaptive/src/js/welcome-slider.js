a7d942b20103d8375e4f3d11f05b625d42931daf
Liliya <liliyavoloshine@gmail.com>
2021-09-23 18:24:10 +0400

    feat: started responsive welcome

--- museum-adaptive/src/js/welcome-slider.js
+++ museum-adaptive/src/js/welcome-slider.js
@@ -5,157 +5,166 @@ const slider = document.querySelector('#welcomeSlider'),
   paginations = document.querySelectorAll('.welcome-slider-pagination-item'),
   counterCurrent = document.querySelector('.welcome-slider-counter-first')
 
-slide(sliderItems, prev, next, paginations)
 
-function slide(items, prev, next, paginations) {
-  let posX1 = 0,
-    posX2 = 0,
-    posInitial,
-    posFinal,
-    threshold = 100,
-    slides = items.querySelectorAll('.slide'),
-    slidesLength = slides.length,
-    slideSize = items.querySelector('.slide').offsetWidth,
-    firstSlide = slides[0],
-    lastSlide = slides[slidesLength - 1],
-    cloneFirst = firstSlide.cloneNode(true),
-    cloneLast = lastSlide.cloneNode(true),
-    index = 0,
-    allowShift = true,
-    activeSlideNum = 1
-  items.appendChild(cloneFirst)
-  items.insertBefore(cloneLast, firstSlide)
-  items.onmousedown = dragStart
-  items.addEventListener('touchstart', dragStart)
-  items.addEventListener('touchend', dragEnd)
-  items.addEventListener('touchmove', dragAction)
-  prev.addEventListener('click', () => shiftSlide(-1))
-  next.addEventListener('click', () => shiftSlide(1))
-  items.addEventListener('transitionend', checkIndex)
-  paginations.forEach(pag => {
-    pag.addEventListener('click', paginate)
+const changeSize = () => {
+  // console.log(slideSize, 'slideSize')
+  console.log(sliderItems.querySelector('.slide').offsetWidth, 'new')
+  // console.log(getComputedStyle(sliderItems.querySelector('.slide')).width, 'new')
+  // console.log(getComputedStyle(sliderItems.querySelector('.slide')).width.match(/\d+/)[0], 'new')
+}
+window.addEventListener('resize', changeSize)
+
+// slide(sliderItems, prev, next, paginations)
+
+let posX1 = 0,
+  posX2 = 0,
+  posInitial,
+  posFinal,
+  threshold = 100,
+  slides = sliderItems.querySelectorAll('.slide'),
+  slidesLength = slides.length,
+  slideSize = sliderItems.querySelector('.slide').offsetWidth,
+  firstSlide = slides[0],
+  lastSlide = slides[slidesLength - 1],
+  cloneFirst = firstSlide.cloneNode(true),
+  cloneLast = lastSlide.cloneNode(true),
+  index = 0,
+  allowShift = true,
+  activeSlideNum = 1
+
+
+sliderItems.appendChild(cloneFirst)
+sliderItems.insertBefore(cloneLast, firstSlide)
+sliderItems.onmousedown = dragStart
+sliderItems.addEventListener('touchstart', dragStart)
+sliderItems.addEventListener('touchend', dragEnd)
+sliderItems.addEventListener('touchmove', dragAction)
+prev.addEventListener('click', () => shiftSlide(-1))
+next.addEventListener('click', () => shiftSlide(1))
+sliderItems.addEventListener('transitionend', checkIndex)
+paginations.forEach(pag => {
+  pag.addEventListener('click', paginate)
+})
+
+console.log(slideSize)
+
+function paginate() {
+  const slideNum = +this.dataset.slide
+  sliderItems.classList.add('shifting')
+
+  if (slideNum === 5) {
+    index = 4
+  } else {
+    index = slideNum
+  }
+
+  sliderItems.style.left = -slideSize * slideNum + 'px'
+
+  allowShift = false
+  activeSlideNum = slideNum
+
+  dotActive()
+  counterActive()
+}
+
+function dragStart(e) {
+  e = e || window.event
+  // e.preventDefault()
+  posInitial = sliderItems.offsetLeft
+
+  if (e.type == 'touchstart') {
+    posX1 = e.touches[0].clientX
+  } else {
+    posX1 = e.clientX
+    document.onmouseup = dragEnd
+    document.onmousemove = dragAction
+  }
+}
+
+function dragAction(e) {
+  e = e || window.event
+
+  if (e.type == 'touchmove') {
+    posX2 = posX1 - e.touches[0].clientX
+    posX1 = e.touches[0].clientX
+  } else {
+    posX2 = posX1 - e.clientX
+    posX1 = e.clientX
+  }
+  sliderItems.style.left = sliderItems.offsetLeft - posX2 + 'px'
+}
+
+function dragEnd(e) {
+  posFinal = sliderItems.offsetLeft
+  if (posFinal - posInitial < -threshold) {
+    shiftSlide(1, 'drag')
+  } else if (posFinal - posInitial > threshold) {
+    shiftSlide(-1, 'drag')
+  } else {
+    sliderItems.style.left = posInitial + 'px'
+  }
+
+  document.onmouseup = null
+  document.onmousemove = null
+}
+
+function shiftSlide(dir, action) {
+  sliderItems.classList.add('shifting')
+
+  if (allowShift) {
+    if (!action) {
+      posInitial = sliderItems.offsetLeft
+    }
+
+    if (dir == 1) {
+      sliderItems.style.left = posInitial - slideSize + 'px'
+      index++
+      if (activeSlideNum !== 5) {
+        activeSlideNum++
+      } else {
+        activeSlideNum = 1
+      }
+    } else if (dir == -1) {
+      sliderItems.style.left = posInitial + slideSize + 'px'
+      index--
+      if (activeSlideNum !== 1) {
+        activeSlideNum--
+      } else {
+        activeSlideNum = 5
+      }
+    }
+  }
+
+  allowShift = false
+  dotActive()
+  counterActive()
+}
+
+function checkIndex() {
+  sliderItems.classList.remove('shifting')
+
+  if (index == -1) {
+    sliderItems.style.left = -(slidesLength * slideSize) + 'px'
+    index = slidesLength - 1
+  }
+
+  if (index == slidesLength) {
+    sliderItems.style.left = -(1 * slideSize) + 'px'
+    index = 0
+  }
+
+  allowShift = true
+}
+
+function dotActive() {
+  paginations.forEach(dot => {
+    dot.classList.contains('active') ? dot.classList.remove('active') : ''
   })
 
-  function paginate() {
-    const slideNum = +this.dataset.slide
-    items.classList.add('shifting')
+  const active = document.querySelector(`.welcome-slider-pagination-item[data-slide="${activeSlideNum}"]`)
+  active.classList.add('active')
+}
 
-    if (slideNum === 5) {
-      index = 4
-    } else {
-      index = slideNum
-    }
-
-    items.style.left = -slideSize * slideNum + 'px'
-
-    allowShift = false
-    activeSlideNum = slideNum
-
-    dotActive()
-    counterActive()
-  }
-
-  function dragStart(e) {
-    e = e || window.event
-    // e.preventDefault()
-    posInitial = items.offsetLeft
-
-    if (e.type == 'touchstart') {
-      posX1 = e.touches[0].clientX
-    } else {
-      posX1 = e.clientX
-      document.onmouseup = dragEnd
-      document.onmousemove = dragAction
-    }
-  }
-
-  function dragAction(e) {
-    e = e || window.event
-
-    if (e.type == 'touchmove') {
-      posX2 = posX1 - e.touches[0].clientX
-      posX1 = e.touches[0].clientX
-    } else {
-      posX2 = posX1 - e.clientX
-      posX1 = e.clientX
-    }
-    items.style.left = items.offsetLeft - posX2 + 'px'
-  }
-
-  function dragEnd(e) {
-    posFinal = items.offsetLeft
-    if (posFinal - posInitial < -threshold) {
-      shiftSlide(1, 'drag')
-    } else if (posFinal - posInitial > threshold) {
-      shiftSlide(-1, 'drag')
-    } else {
-      items.style.left = posInitial + 'px'
-    }
-
-    document.onmouseup = null
-    document.onmousemove = null
-  }
-
-  function shiftSlide(dir, action) {
-    items.classList.add('shifting')
-
-    if (allowShift) {
-      if (!action) {
-        posInitial = items.offsetLeft
-      }
-
-      if (dir == 1) {
-        items.style.left = posInitial - slideSize + 'px'
-        index++
-        if (activeSlideNum !== 5) {
-          activeSlideNum++
-        } else {
-          activeSlideNum = 1
-        }
-      } else if (dir == -1) {
-        items.style.left = posInitial + slideSize + 'px'
-        index--
-        if (activeSlideNum !== 1) {
-          activeSlideNum--
-        } else {
-          activeSlideNum = 5
-        }
-      }
-    }
-
-    allowShift = false
-    dotActive()
-    counterActive()
-  }
-
-  function checkIndex() {
-    items.classList.remove('shifting')
-
-    if (index == -1) {
-      items.style.left = -(slidesLength * slideSize) + 'px'
-      index = slidesLength - 1
-    }
-
-    if (index == slidesLength) {
-      items.style.left = -(1 * slideSize) + 'px'
-      index = 0
-    }
-
-    allowShift = true
-  }
-
-  function dotActive() {
-    paginations.forEach(dot => {
-      dot.classList.contains('active') ? dot.classList.remove('active') : ''
-    })
-
-    const active = document.querySelector(
-      `.welcome-slider-pagination-item[data-slide="${activeSlideNum}"]`
-    )
-    active.classList.add('active')
-  }
-
-  function counterActive() {
-    counterCurrent.textContent = `0${activeSlideNum}`
-  }
+function counterActive() {
+  counterCurrent.textContent = `0${activeSlideNum}`
 }
