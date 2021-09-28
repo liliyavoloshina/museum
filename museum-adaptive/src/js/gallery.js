f02184350764576c74a1c110bfb2bc0ee90fa4d9
Liliya <liliyavoloshine@gmail.com>
2021-09-28 14:52:57 +0400

    feat: add another way to check webp

--- museum-adaptive/src/js/gallery.js
+++ museum-adaptive/src/js/gallery.js
@@ -1,80 +1,64 @@
 const gallery = document.querySelector('.gallery-container')
 
-// var Modernizr = require('./modernizr')
+let sources = []
 
-// modernizr.build(
-//   {
-//     minify: true,
-//     options: ['setClasses'],
-//     'feature-detects': ['test/img/webp']
-//   },
-//   function(result) {
-//     console.log(result) // the build
-//   }
-// )
-// Modernizr.on('webp', function (result) {
-//   // `result == Modernizr.webp`
-//   console.log(result);  // either `true` or `false`
-//   if (result) {
-//     // Has WebP support
-//   }
-//   else {
-//     // No WebP support
-//   }
-// })
+const isWebpSupport = checkWebpSupport()
 
-// modernizr.on( 'webp', function( result ) {
-//   console.log(result)
-//   if ( result ) {
-//     console.log(result); // true or false;
-//   }
-// });
-
-const sources = [
-  '../img/gallery/galery1.jpg',
-  '../img/gallery/galery2.jpg',
-  '../img/gallery/galery3.jpg',
-  '../img/gallery/galery4.jpg',
-  '../img/gallery/galery5.jpg',
-  '../img/gallery/galery6.jpg',
-  '../img/gallery/galery7.jpg',
-  '../img/gallery/galery8.jpg',
-  '../img/gallery/galery9.jpg',
-  '../img/gallery/galery10.jpg',
-  '../img/gallery/galery11.jpg',
-  '../img/gallery/galery12.jpg',
-  '../img/gallery/galery13.jpg',
-  '../img/gallery/galery14.jpg',
-  '../img/gallery/galery15.jpg'
-]
-
+getSources()
 shuffle(sources)
+insertImages(sources)
 
-sources.map(pic => {
-  const imgWrapper = document.createElement('div')
-  imgWrapper.classList.add('gallery-wrapper')
+function checkWebpSupport() {
+  const canvas = document.createElement('canvas')
+  if (!!(canvas.getContext && canvas.getContext('2d'))) {
+    // was able or not to get WebP representation
+    return canvas.toDataURL('image/webp').indexOf('data:image/webp') == 0
+  } else {
+    // very old browser like IE 8, canvas not supported
+    return false
+  }
+}
 
-  const img = document.createElement('img')
-  img.classList.add('gallery-picture')
-  img.src = pic
-  img.alt = 'Gallery Picture'
-  gallery.append(imgWrapper)
-  imgWrapper.append(img)
-
-  img.onload = function() {
-    const height = this.height
-
-    if (height >= 570) {
-      imgWrapper.classList.add('long')
-    } else if (height >= 456 && height < 570) {
-      imgWrapper.classList.add('medium')
-    } else if (height < 456) {
-      imgWrapper.classList.add('short')
+function getSources() {
+  if (isWebpSupport) {
+    for (let i = 1; i < 16; i++) {
+      sources.push(`./img/gallery/galery${i}.webp`)
+    }
+  } else {
+    for (let i = 1; i < 16; i++) {
+      sources.push(`./img/gallery/galery${i}.jpg`)
     }
   }
-})
+}
+
+function insertImages(sources) {
+  sources.map(pic => {
+    const imgWrapper = document.createElement('div')
+    imgWrapper.classList.add('gallery-wrapper')
+
+    const img = document.createElement('img')
+    img.classList.add('gallery-picture')
+    img.src = pic
+    img.alt = 'Gallery Picture'
+    gallery.append(imgWrapper)
+    imgWrapper.append(img)
+
+    img.onload = function() {
+      const height = this.height
+
+      if (height >= 570) {
+        imgWrapper.classList.add('long')
+      } else if (height >= 456 && height < 570) {
+        imgWrapper.classList.add('medium')
+      } else if (height < 456) {
+        imgWrapper.classList.add('short')
+      }
+    }
+  })
+}
 
 function shuffle(array) {
+  console.log(sources)
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[array[i], array[j]] = [array[j], array[i]]
