f41b1e395d2b46692222120f46cb98646bf54baa
Liliya <liliyavoloshine@gmail.com>
2021-09-30 20:42:41 +0400

    feat: add dots style and cur number for welcome slider

--- museum-adaptive/src/js/welcome-slider.js
+++ museum-adaptive/src/js/welcome-slider.js
@@ -1,10 +1,10 @@
 import { tns } from 'tiny-slider/src/tiny-slider'
 
-const slider2 = tns({
+const welcomeSlider = tns({
   container: '#welcomeSlider',
   loop: true,
   mouseDrag: true,
-  speed: 2000,
+  speed: 1500,
   lazyload: true,
   navContainer: '#welcomePag',
   navAsThumbnails: true,
@@ -12,168 +12,10 @@ const slider2 = tns({
   nextButton: '#welcomeNext'
 })
 
-// const slider = document.querySelector('#welcomeSlider'),
-//   sliderItems = document.querySelector('#welcomeSlides'),
-//   prev = document.querySelector('#welcomePrev'),
-//   next = document.querySelector('#welcomeNext'),
-//   paginations = document.querySelectorAll('.welcome-slider-pagination-item'),
-//   counterCurrent = document.querySelector('.welcome-slider-counter-first')
+const curSlideNum = document.querySelector('#currentSlide')
 
-// const changeSize = () => {
-//   slideSize = sliderItems.querySelector('.slide').offsetWidth
-//   document.documentElement.style.setProperty(
-//     '--scrollbar-width',
-//     window.innerWidth - document.documentElement.clientWidth + 'px'
-//   )
-// }
-// window.addEventListener('resize', changeSize)
-
-// let posX1 = 0,
-//   posX2 = 0,
-//   posInitial,
-//   posFinal,
-//   threshold = 100,
-//   slides = sliderItems.querySelectorAll('.slide'),
-//   slidesLength = slides.length,
-//   slideSize = sliderItems.querySelector('.slide').offsetWidth,
-//   firstSlide = slides[0],
-//   lastSlide = slides[slidesLength - 1],
-//   cloneFirst = firstSlide.cloneNode(true),
-//   cloneLast = lastSlide.cloneNode(true),
-//   index = 0,
-//   allowShift = true,
-//   activeSlideNum = 1
-
-// sliderItems.appendChild(cloneFirst)
-// sliderItems.insertBefore(cloneLast, firstSlide)
-// sliderItems.onmousedown = dragStart
-// sliderItems.addEventListener('touchstart', dragStart)
-// sliderItems.addEventListener('touchend', dragEnd)
-// sliderItems.addEventListener('touchmove', dragAction)
-// prev.addEventListener('click', () => shiftSlide(-1))
-// next.addEventListener('click', () => shiftSlide(1))
-// sliderItems.addEventListener('transitionend', checkIndex)
-// paginations.forEach(pag => {
-//   pag.addEventListener('click', paginate)
-// })
-
-// function paginate() {
-//   const slideNum = +this.dataset.slide
-//   sliderItems.classList.add('shifting')
-
-//   if (slideNum === 5) {
-//     index = 4
-//   } else {
-//     index = slideNum
-//   }
-
-//   sliderItems.style.left = -slideSize * slideNum + 'px'
-
-//   allowShift = false
-//   activeSlideNum = slideNum
-
-//   dotActive()
-//   counterActive()
-// }
-
-// function dragStart(e) {
-//   e = e || window.event
-//   e.preventDefault()
-//   posInitial = sliderItems.offsetLeft
-
-//   if (e.type == 'touchstart') {
-//     posX1 = e.touches[0].clientX
-//   } else {
-//     posX1 = e.clientX
-//     document.onmouseup = dragEnd
-//     document.onmousemove = dragAction
-//   }
-// }
-
-// function dragAction(e) {
-//   e = e || window.event
-
-//   if (e.type == 'touchmove') {
-//     posX2 = posX1 - e.touches[0].clientX
-//     posX1 = e.touches[0].clientX
-//   } else {
-//     posX2 = posX1 - e.clientX
-//     posX1 = e.clientX
-//   }
-//   sliderItems.style.left = sliderItems.offsetLeft - posX2 + 'px'
-// }
-
-// function dragEnd(e) {
-//   posFinal = sliderItems.offsetLeft
-//   if (posFinal - posInitial < -threshold) {
-//     shiftSlide(1, 'drag')
-//   } else if (posFinal - posInitial > threshold) {
-//     shiftSlide(-1, 'drag')
-//   } else {
-//     sliderItems.style.left = posInitial + 'px'
-//   }
-
-//   document.onmouseup = null
-//   document.onmousemove = null
-// }
-
-// function shiftSlide(dir, action) {
-//   sliderItems.classList.add('shifting')
-
-//   if (allowShift) {
-//     if (!action) {
-//       posInitial = sliderItems.offsetLeft
-//     }
-
-//     if (dir == 1) {
-//       sliderItems.style.left = posInitial - slideSize + 'px'
-//       index++
-//       if (activeSlideNum !== 5) {
-//         activeSlideNum++
-//       } else {
-//         activeSlideNum = 1
-//       }
-//     } else if (dir == -1) {
-//       sliderItems.style.left = posInitial + slideSize + 'px'
-//       index--
-//       if (activeSlideNum !== 1) {
-//         activeSlideNum--
-//       } else {
-//         activeSlideNum = 5
-//       }
-//     }
-//   }
-
-//   allowShift = false
-//   dotActive()
-//   counterActive()
-// }
-
-// function checkIndex() {
-//   sliderItems.classList.remove('shifting')
-
-//   if (index == -1) {
-//     sliderItems.style.left = -(slidesLength * slideSize) + 'px'
-//     index = slidesLength - 1
-//   }
-
-//   if (index == slidesLength) {
-//     sliderItems.style.left = -(1 * slideSize) + 'px'
-//     index = 0
-//   }
-
-//   allowShift = true
-// }
-
-// function dotActive() {
-//   paginations.forEach(dot => {
-//     dot.classList.contains('active') ? dot.classList.remove('active') : ''
-//   })
-
-//   const active = document.querySelector(`.welcome-slider-pagination-item[data-slide="${activeSlideNum}"]`)
-//   active.classList.add('active')
-// }
-
-// function counterActive() {
-//   counterCurrent.textContent = `0${activeSlideNum}`
-// }
+welcomeSlider.events.on('indexChanged', () => {
+  const info = welcomeSlider.getInfo()
+  const indexCurr = info.index
+  curSlideNum.textContent = `0${indexCurr}`
+})
